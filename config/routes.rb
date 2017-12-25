467e53a6b411e63e59b25b12d847df751a14fdc7
boatengfrankenstein <boatengfrankenstein@gmail.com>
2017-12-25 02:11:06 -0500

    Chapter 5. Build Dynamic UI with AngularJS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'customers/index'
-
+  get "angular_test", to: "angular_test#index"
   devise_for :users
   root 'dashboard#index'
   resources :customers, only: [ :index ]
