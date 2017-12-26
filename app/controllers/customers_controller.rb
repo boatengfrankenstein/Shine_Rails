03d620334a59e7530050b0d63093811d6b1b360f
boatengfrankenstein <boatengfrankenstein@gmail.com>
2017-12-25 22:39:39 -0500

    chapter 5: changing our search to use typehehad

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,8 +11,15 @@ class CustomersController < ApplicationController
 				customer_search_term.where_args).
 			order(customer_search_term.order).
 			 offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
+
 		else
 			@customers = []
 		end
+
+		respond_to do |format|
+       	 format.html {}
+         format.json { render json: @customers }
+     end
+
 	end
 end
